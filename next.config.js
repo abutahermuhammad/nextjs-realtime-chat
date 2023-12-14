dd016381bc2ccd3efab2bee7a5a4221ea9a724df
Abu Taher Muhamamd <abut1081@gmail.com>
2023-12-14 12:38:28 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com']
-  }
+ // images: {
+    // domains: ['lh3.googleusercontent.com']
+ // }
 }
 
 module.exports = nextConfig
